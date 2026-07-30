b093174f8a13e6d8b09fe3d31a3458389698ad8c
Akunne felix <akunnefelix@gmail.com>
2026-07-30 18:44:05 +0100

    navbar

--- frontend/app.js
+++ frontend/app.js
@@ -303,13 +303,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const navToggle = document.getElementById("navToggle");
-    const navLinks = document.getElementById("navLinks");
-
-    if (navToggle && navLinks) {
-        navToggle.addEventListener("click", () => {
-            navLinks.classList.toggle("nav-open");
-        });
-    }
-});
